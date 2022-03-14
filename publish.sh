2c1da38dbbc8cfe601d21a4fa959b65291a14eb8
TheCheshireFox <ddrambler@gmail.com>
2022-03-15 02:19:12 +0300

    Improve app publishing

--- publish.sh
+++ publish.sh
@@ -3,18 +3,32 @@
 cd "$(dirname "$0")"
 
 rm -rf publish
-mkdir -p publish
+mkdir -p publish/{portable,standart}/{Release,pdb,dist/{bin,}}
 
-# x64
-dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true --self-contained true -o publish/ TcfBackup/TcfBackup.csproj
-dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true --self-contained true -o publish/ TcfGoogleDriveAuth/TcfGoogleDriveAuth.csproj
+function dotnet_publish()
+{
+  dotnet publish -c Release -r linux-x64 -nologo -clp:NoSummary -verbosity:quiet "$@" TcfBackup/TcfBackup.csproj
+}
 
-mkdir publish/dist
-mkdir publish/pdb
+dotnet_publish -o publish/portable/Release -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:SelfContained=true -p:EnableCompressionInSingleFile=true -p:PublishTrimmed=true --self-contained true -nowarn:IL2104,IL2087,IL2091,IL2026,IL2090
+dotnet_publish -o publish/standart/Release --no-self-contained
 
-mv -v publish/*.pdb publish/pdb/
-mv -v publish/tcf-backup publish/dist/
-mv -v publish/tcf-google-drive-auth publish/dist/
-mv -v publish/credentials.json publish/dist/
-cp -v Makefile publish/dist/
+mv publish/portable/Release/*.pdb publish/portable/pdb
+mv publish/portable/Release/tcf-backup publish/portable/dist/bin
+cp Makefile.portable publish/portable/dist/Makefile
 
+printf '\033[0;31m'
+find publish/portable/Release/* -print 2>/dev/null
+printf '\033[0m'
+
+mv publish/standart/Release/*.pdb publish/standart/pdb
+mv publish/standart/Release/*.dll publish/standart/dist/bin
+mv publish/standart/Release/*.so publish/standart/dist/bin
+mv publish/standart/Release/tcf-backup.deps.json publish/standart/dist/bin
+mv publish/standart/Release/tcf-backup.runtimeconfig.json publish/standart/dist/bin
+mv publish/standart/Release/tcf-backup publish/standart/dist/bin
+cp Makefile.standart publish/standart/dist/Makefile
+
+printf '\033[0;31m'
+find publish/standart/Release/* -print 2>/dev/null
+printf '\033[0m'
