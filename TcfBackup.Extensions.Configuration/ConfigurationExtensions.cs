ea8e17abb70ad61171ca78570328541fae9e8a60
TheCheshireFox <ddrambler@gmail.com>
2024-01-28 06:36:26 +0100

    Remove unused methods

--- TcfBackup.Extensions.Configuration/ConfigurationExtensions.cs
+++ TcfBackup.Extensions.Configuration/ConfigurationExtensions.cs
@@ -16,33 +16,11 @@ public static class ConfigurationExtensions
     
     public static bool ContainsKey(this IConfiguration configuration, string name) => configuration.GetSection(name).Exists();
 
-    public static object Get(this IConfiguration configuration, Func<IConfiguration, Type> typeSelector, string key)
-    {
-        var cfg = configuration.GetSection(key);
-        if (!cfg.Exists())
-        {
-            throw new KeyNotFoundException(key);
-        }
-
-        return cfg.Get(typeSelector);
-    }
-
-    public static object Get(this IConfiguration configuration, Func<IConfiguration, Type> typeSelector)
-    {
-        var type = typeSelector(configuration);
-        if (type == null)
-        {
-            throw new KeyNotFoundException();
-        }
-
-        return configuration.Get(type) ?? throw new KeyNotFoundException(type.Name);
-    }
-
     public static IConfigurationSection GetSection(this IConfiguration configuration, string name, StringComparison nameComparison)
     {
         return configuration
                    .GetChildren()
                    .FirstOrDefault(c => string.Equals(c.Key, name, nameComparison))
-               ?? (IConfigurationSection)new ConfigurationSection((IConfigurationRoot)configuration, name);
+               ?? new ConfigurationSection((IConfigurationRoot)configuration, name);
     }
 }
