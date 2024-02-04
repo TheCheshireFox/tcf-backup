99d31fefac7907cd294eeb186a0a2a62edcc7059
TheCheshireFox <ddrambler@gmail.com>
2024-02-04 06:21:18 +0100

    Add timestamp to logs

--- TcfBackup/LoggerOptions.cs
+++ TcfBackup/LoggerOptions.cs
@@ -6,7 +6,7 @@ namespace TcfBackup;
 public class LoggerOptions
 {
     public LogEventLevel LogLevel { get; private set; } = LogEventLevel.Information;
-    public string Format => "[{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
+    public string Format => "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
 
     public void Fill(GenericOptions opts)
     {
